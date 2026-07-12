291e3efc8a66850b61c045e1a0ff4ba9580926c3
Eric.Chin.AMD <ericchin@amd.com>
2026-07-13 01:05:06 +0800

    Update moe.py

--- atom/model_ops/moe.py
+++ atom/model_ops/moe.py
@@ -14,7 +14,11 @@ from aiter.fused_moe import fused_moe
 from aiter.jit.utils.chip_info import get_gfx
 from aiter.jit.utils.torch_guard import torch_compile_guard
 from aiter.ops.flydsl.moe_common import GateMode
-from aiter.ops.shuffle import moe_shuffle_scale, shuffle_weight
+from aiter.ops.shuffle import shuffle_weight
+try:  # aiter>=new exposes moe_shuffle_scale; older/sgl-dev aiter only has shuffle_scale (equivalent on non-gfx1250)
+    from aiter.ops.shuffle import moe_shuffle_scale
+except ImportError:
+    from aiter.ops.shuffle import shuffle_scale as moe_shuffle_scale
 from atom.config import (
     Config,
     QuantizationConfig,
